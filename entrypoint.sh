57f603211fdd3f0e46c4a20fc4ca3aaeb1f99a62
f0rkz <f0rkz@f0rkznet.net>
2022-04-07 22:05:26 -0400

    Add whitelisted mods

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,14 @@
 
 unzip -ao /openworld.zip -d /openworld
 
+rm -f /openworld/Mods || :
+rm -f /openworld/Whitelisted\ Mods || :
+
 mkdir -p /openworld/Mods
 mkdir -p /tmp/mods
+mkdir -p /tmp/whitelisted_mods
 
+# We need hugslib, harmony, and open world
 for mod_id in `cat /tmp/mods.base.txt`;
 do
     /steamcmd/steamcmd.sh \
@@ -12,10 +17,10 @@ do
         +login anonymous \
         +workshop_download_item 294100 $mod_id \
         +quit
-    rm -f /openworld/Mods/$mod_id || :
     cp -paxv /tmp/mods/steamapps/workshop/content/294100/$mod_id /openworld/Mods/
 done
 
+# Required mods
 if [ -f '/openworld/mods.txt' ]; then
     for mod_id in `cat /openworld/mods.txt`;
     do
@@ -24,8 +29,20 @@ if [ -f '/openworld/mods.txt' ]; then
             +login anonymous \
             +workshop_download_item 294100 $mod_id \
             +quit
-        rm -f /openworld/Mods/$mod_id || :
         cp -paxv /tmp/mods/steamapps/workshop/content/294100/$mod_id /openworld/Mods/
+    done
+fi
+
+# Whitelisted mods
+if [ -f '/openworld/mod_whitelist.txt' ]; then
+    for mod_id in `cat /openworld/mod_whitelist.txt`;
+    do
+        /steamcmd/steamcmd.sh \
+            +force_install_dir /tmp/mods \
+            +login anonymous \
+            +workshop_download_item 294100 $mod_id \
+            +quit
+        cp -paxv /tmp/mods/steamapps/workshop/content/294100/$mod_id /openworld/Whitelisted\ Mods/
     done
 fi
 
