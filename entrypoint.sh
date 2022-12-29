293c7fc1aefe7c17182d88259df688cf7f02c728
Kirk Main <kirk.j.main+github@gmail.com>
2022-12-29 09:58:19 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-cd /openworld
+apt install -y --no-install-recommends \
+        curl \
+        lib32gcc1 \
+        unzip \
+        ca-certificates \
+        libicu66 \
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y install screen
+apt -y install wget
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+dpkg -i ./libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+rm libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+wget https://dot.net/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 3.1
+rm dotnet-install.sh
+mkdir /openworld
+wget github.com/TastyLollipop/OpenWorld/releases/latest/download/LinuxX64.zip
+unzip LinuxX64.zip –d /openworld
+chmod +x /openworld/run.sh
+chmod +x /openworld/Open\ World\ Server
+mkdir /steamcmd
+wget steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+tar -zxvf steamcmd_linux.tar.gz /steamcmd
+/steamcmd/steamcmd.sh +quit
 
 # Required mods
 if [ -f '/openworld/mods.txt' ]; then
@@ -30,5 +56,4 @@ if [ -f '/openworld/mod_whitelist.txt' ]; then
     done
 fi
 
-which screen
 screen -S /openworld/run.sh
