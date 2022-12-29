331268bae4645d8cb59802a736332330bdb540d5
Kirk Main <kirk.j.main+github@gmail.com>
2022-12-29 09:42:15 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-unzip -ao /openworld.zip -d /openworld
-
-rm -f /openworld/Mods || :
-rm -f /openworld/Whitelisted\ Mods || :
-
-mkdir -p /openworld/Mods
 mkdir -p /tmp/mods
 mkdir -p /tmp/whitelisted_mods
-
-# We need hugslib, harmony, and open world
-for mod_id in `cat /tmp/mods.base.txt`;
-do
-    /steamcmd/steamcmd.sh \
-        +force_install_dir /tmp/mods \
-        +login anonymous \
-        +workshop_download_item 294100 $mod_id \
-        +quit
-    cp -paxv /tmp/mods/steamapps/workshop/content/294100/$mod_id /openworld/Mods/
-done
 
 # Required mods
 if [ -f '/openworld/mods.txt' ]; then
@@ -29,7 +12,7 @@ if [ -f '/openworld/mods.txt' ]; then
             +login anonymous \
             +workshop_download_item 294100 $mod_id \
             +quit
-        cp -paxv /tmp/mods/steamapps/workshop/content/294100/$mod_id /openworld/Mods/
+        cp -paxv /tmp/mods/steamapps/workshop/content/294100/$mod_id /openworld/Enforced\ Mods/
     done
 fi
 
@@ -46,4 +29,4 @@ if [ -f '/openworld/mod_whitelist.txt' ]; then
     done
 fi
 
-screen -S openworld /root/.dotnet/dotnet /openworld/Open\ World\ Server.dll
+screen -S /openworld/run.sh
